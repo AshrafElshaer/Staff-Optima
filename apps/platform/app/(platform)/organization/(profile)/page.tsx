cef7669769942d547fde035fc3eb5620490a2a85
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-03-17 14:17:21 -0500

    loading page for org profile

--- apps/platform/app/(platform)/organization/(profile)/page.tsx
+++ apps/platform/app/(platform)/organization/(profile)/page.tsx
@@ -2,9 +2,11 @@ import { OrganizationProfileForm } from "@/features/organization/profile/profile
 import { auth } from "@/lib/auth/auth";
 import { getUserOrganization } from "@optima/database/queries";
 import { headers } from "next/headers";
+
 export default async function OrganizationProfilePage() {
 	// const headersList = await headers();
 	// const userId = headersList.get("x-user-id");
+
 	const session = await auth.api.getSession({
 		headers: await headers(),
 	});
