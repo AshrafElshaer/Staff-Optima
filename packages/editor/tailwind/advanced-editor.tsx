a4aa4c974242e32a88f3771c394172c6c93f8cb3
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-03-16 22:38:05 -0500

    novel editor v1 test

--- packages/editor/tailwind/advanced-editor.tsx
+++ packages/editor/tailwind/advanced-editor.tsx
@@ -75,6 +75,7 @@ const TailwindAdvancedEditor = ({
 				<EditorContent
 					immediatelyRender={false}
 					initialContent={initialContent ?? undefined}
+					
 					extensions={extensions}
 					className="relative h-full w-full max-w-screen-lg border-muted bg-background sm:mb-[calc(20vh)] sm:rounded-lg sm:border sm:shadow-lg"
 					editorProps={{
