5ad9ccba075d7f1a48dd30d4d1c913ed9bb10099
Ashraf Elshaer <ashrafelshaer98@icloud.com>
2025-03-24 09:56:36 -0500

    protect onboarding pages from duplicates

--- apps/platform/app/onboarding/organization/page.tsx
+++ apps/platform/app/onboarding/organization/page.tsx
@@ -1,11 +1,27 @@
 import { OrganizationOnboarding } from "@/features/onboarding/views/organization-onboarding";
+import { createServerClient } from "@/lib/supabase/server";
+import { getOrganizationById } from "@optima/supabase/queries";
 import type { Metadata } from "next";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
 	title: "Onboarding Organization",
 	description: "Setup your organization",
 };
 
-export default function OnboardingOrganizationPage() {
+export default async function OnboardingOrganizationPage() {
+	const supabase = await createServerClient();
+	const headersList = await headers();
+	const organization_id = headersList.get("x-organization-id");
+	const { data: organization } = await getOrganizationById(
+		supabase,
+		organization_id ?? "",
+	);
+
+	if (organization) {
+		redirect("/onboarding/congrats");
+	}
+
 	return <OrganizationOnboarding />;
 }
--- apps/platform/app/onboarding/user/page.tsx
+++ apps/platform/app/onboarding/user/page.tsx
@@ -1,6 +1,9 @@
 import { UserOnboarding } from "@/features/onboarding/views/user-onboarding";
+import { createServerClient } from "@/lib/supabase/server";
 import { getCountryCode } from "@optima/location";
+import { getCurrentUser } from "@optima/supabase/queries";
 import type { Metadata } from "next";
+import { redirect } from "next/navigation";
 
 export const metadata: Metadata = {
 	title: "Onboarding User",
@@ -9,6 +12,13 @@ export const metadata: Metadata = {
 
 export default async function OnboardingUserPage() {
 	const countryCode = await getCountryCode();
+
+	const supabase = await createServerClient();
+	const { data: user } = await getCurrentUser(supabase);
+
+	if (user) {
+		redirect("/onboarding/organization");
+	}
 
 	return <UserOnboarding countryCode={countryCode} />;
 }
